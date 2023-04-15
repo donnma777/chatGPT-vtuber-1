fd29ffb90ee54e5fe555975dbb0b2f8d5a10a0fa
donnma777 <96525816+donnma777@users.noreply.github.com>
2023-04-16 00:20:48 +0900

    アプリ連携コード編集

--- src/App.js
+++ src/App.js
@@ -9,13 +9,71 @@ const App = () => {
   const youtube_apikey = "";
 
   // const LiveComent = await(Live);
-  
+
   // メッセージの状態管理用
   const [youtubeUrl, setYoutubeUrl] = useState('');
   const [youtubeCommentAcquisitionFunc, setYoutubeCommentAcquisitionFunc] = useState('');
   const [youtubeCommentData, setYoutubeCommentData] = useState('');
   const [readingCommentStartFlg, setReadingCommentStartFlg] = useState(false);
   const [reading_comment_StopFlg, setReading_comment_StopFlg] = useState(false);
+
+  // 音声再生
+  //まだ本コードは未完成。再生出来ない。
+  //VOICEVOX公式サイト
+  //https://voicevox.hiroshiba.jp/
+  //VOICEVOXのソフトを立ち上げるとローカルサーバーが立ち上がりAPIが実行出来る。
+
+  // const [voice, setVoice] = useState(false);
+  // const [voiceDate, setVoiceData] = useState(false);
+
+  // setVoice = async () => {
+  //   const parAmaudioQuery = "テスト音声です";
+  //   // VOICEVOXのローカルサーバーURL
+  //   const VOICE_VOX_API_URL = "http://localhost:50021";
+  //   // VOICEVOXのSpeakerID
+  //   const VOICEVOX_SPEAKER_ID = '10';
+  //   const appUrl = axios.post(VOICE_VOX_API_URL + '/audio_query?speaker=' + VOICEVOX_SPEAKER_ID + '&text=' + parAmaudioQuery)
+  //
+  //   if (appUrl.status === 200) {
+  //     setVoiceData(appUrl.data);
+  //   }
+  //   console.log(voice);
+  // }
+  // console.log(voiceDate);
+
+
+
+  // VtuberStudio連携
+  //仕様書
+  // https://github.com/DenchiSoft/VTubeStudio#requesting-list-of-hotkeys-available-in-current-or-other-vts-model
+  //http://localhost:8001/
+
+  //まだ未完成
+  // const [vtuberStudio, setVtuberStudio] = useState(false);
+  // setVtuberStudio = async () => {
+  //   const VtuberStudio_API_URL = "http://localhost:8001";
+  //   //リクエストに付加するヘッダーの定義
+  //   const headers = {
+  //     'Content-Type': 'application/json',
+  //     'any-header':
+  //     {
+  //       apiName: "VTubeStudioPublicAPI",
+  //       apiVersion: "1.0",
+  //       requestID: "SomeID",
+  //       messageType: "HotkeyTriggerRequest",
+  //       data: {
+  //         hotkeyID: "HotkeyNameOrUniqueIdOfHotkeyToExecute",
+  //         itemInstanceID: "Optional_ItemInstanceIdOfLive2DItemToTriggerThisHotkeyFor"
+  //       }
+  //     }
+  //   }
+  //   axios.post(VtuberStudio_API_URL, { headers: headers });
+  // }
+
+
+
+
+
 
   // 回答の状態管理用
   const [answer, setAnswer] = useState('');
@@ -46,10 +104,10 @@ const App = () => {
 
     const LiveUrl = new URL(youtubeUrl);
     const queryParams = new URLSearchParams(LiveUrl.search);
-    const youtubeVideoUrl  = 'https://www.googleapis.com/youtube/v3/videos';
-    const params = {'key': youtube_apikey, 'id': queryParams.get('v'), 'part': 'liveStreamingDetails'};
+    const youtubeVideoUrl = 'https://www.googleapis.com/youtube/v3/videos';
+    const params = { 'key': youtube_apikey, 'id': queryParams.get('v'), 'part': 'liveStreamingDetails' };
 
-    const youtubeVideoResponse = await axios.get(youtubeVideoUrl, {params: params});
+    const youtubeVideoResponse = await axios.get(youtubeVideoUrl, { params: params });
 
     if (youtubeVideoResponse.status !== 200) {
       return;
@@ -62,14 +120,14 @@ const App = () => {
 
     setYoutubeCommentAcquisitionFunc(setInterval(async () => {
 
-      const LiveChatUrl    = 'https://www.googleapis.com/youtube/v3/liveChat/messages'
-      const param = {'key': youtube_apikey, 'liveChatId': channelId, 'part': 'id,snippet,authorDetails'}
-    
+      const LiveChatUrl = 'https://www.googleapis.com/youtube/v3/liveChat/messages'
+      const param = { 'key': youtube_apikey, 'liveChatId': channelId, 'part': 'id,snippet,authorDetails' }
+
       const LiveChatResponse = await axios.get(LiveChatUrl, { params: param });
 
       console.log(LiveChatResponse)
 
-      if(LiveChatResponse.status === 200) {
+      if (LiveChatResponse.status === 200) {
         setYoutubeCommentData(LiveChatResponse.data.items);
       }
     }, 2000))
@@ -81,7 +139,7 @@ const App = () => {
 
   const reading_comment_Stop = async () => {
 
-    if(reading_comment_StopFlg) {
+    if (reading_comment_StopFlg) {
       return;
     }
 
@@ -91,6 +149,11 @@ const App = () => {
     setReading_comment_StopFlg(true);
 
   }
+
+
+
+
+
 
   // チャットフォームの表示
   return (
@@ -107,11 +170,11 @@ const App = () => {
         </label>
       </form>
       <div>
-          <button onClick={reading_comment_start} disabled={readingCommentStartFlg}>コメント読み取り開始</button>
-        </div>
+        <button onClick={reading_comment_start} disabled={readingCommentStartFlg}>コメント読み取り開始</button>
+      </div>
       <div>
-          <button onClick={reading_comment_Stop} disabled={reading_comment_StopFlg}>コメント読み取り停止</button>
-        </div>
+        <button onClick={reading_comment_Stop} disabled={reading_comment_StopFlg}>コメント読み取り停止</button>
+      </div>
       {youtubeCommentData && (
         youtubeCommentData?.map((data) => (
           <div key={data.id}>{data.snippet.textMessageDetails.messageText}</div>
