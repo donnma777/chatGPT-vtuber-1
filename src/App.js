67d347a5db19bc44790eb3a1f79d783bd9ac2390
arakirai <arakirai1128@gmail.com>
2023-04-15 16:46:07 +0900

    youtubeコメント読み取り機能実装

--- src/App.js
+++ src/App.js
@@ -1,54 +1,128 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { chat } from './Chat';  // chat.js のインポート
 import { Live } from './utils/Live';
 
+import axios from 'axios';
+
 const App = () => {
+
+  const youtube_apikey = "";
 
   // const LiveComent = await(Live);
   
   // メッセージの状態管理用
-  const [message, setMessage] = useState('');
+  const [youtubeUrl, setYoutubeUrl] = useState('');
+  const [youtubeCommentAcquisitionFunc, setYoutubeCommentAcquisitionFunc] = useState('');
+  const [youtubeCommentData, setYoutubeCommentData] = useState('');
+  const [readingCommentStartFlg, setReadingCommentStartFlg] = useState(false);
+  const [reading_comment_StopFlg, setReading_comment_StopFlg] = useState(false);
+
   // 回答の状態管理用
   const [answer, setAnswer] = useState('');
 
   // メッセージの格納
   const handleMessageChange = (event) => {
-    setMessage(event.target.value);
+    setYoutubeUrl(event.target.value);
   }
 
   // 「質問」ボタンを押したときの処理
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
 
-    // chat.js にメッセージを渡して API から回答を取得
-    // const responseText = await chat(LiveComent);
+  //   // chat.js にメッセージを渡して API から回答を取得
+  //   // const responseText = await chat(LiveComent);
 
-    // 回答の格納
-    // setAnswer(responseText);
+  //   // 回答の格納
+  //   // setAnswer(responseText);
+  // }
+
+  const reading_comment_start = async (event) => {
+
+    console.log("読み取り開始")
+
+    if (readingCommentStartFlg) {
+      return;
+    }
+
+    const LiveUrl = new URL(youtubeUrl);
+    const queryParams = new URLSearchParams(LiveUrl.search);
+    const youtubeVideoUrl  = 'https://www.googleapis.com/youtube/v3/videos';
+    const params = {'key': youtube_apikey, 'id': queryParams.get('v'), 'part': 'liveStreamingDetails'};
+
+    const youtubeVideoResponse = await axios.get(youtubeVideoUrl, {params: params});
+
+    if (youtubeVideoResponse.status !== 200) {
+      return;
+    }
+
+    // console.log(youtubeVideoResponse)
+
+    const channelId = youtubeVideoResponse.data.items[0].liveStreamingDetails.activeLiveChatId;
+    // console.log(channelId)
+
+    setYoutubeCommentAcquisitionFunc(setInterval(async () => {
+
+      const LiveChatUrl    = 'https://www.googleapis.com/youtube/v3/liveChat/messages'
+      const param = {'key': youtube_apikey, 'liveChatId': channelId, 'part': 'id,snippet,authorDetails'}
+    
+      const LiveChatResponse = await axios.get(LiveChatUrl, { params: param });
+
+      console.log(LiveChatResponse)
+
+      if(LiveChatResponse.status === 200) {
+        setYoutubeCommentData(LiveChatResponse.data.items);
+      }
+    }, 2000))
+
+    setReading_comment_StopFlg(false);
+    setReadingCommentStartFlg(true);
+
+  }
+
+  const reading_comment_Stop = async () => {
+
+    if(reading_comment_StopFlg) {
+      return;
+    }
+
+    clearInterval(youtubeCommentAcquisitionFunc)
+
+    setReadingCommentStartFlg(false);
+    setReading_comment_StopFlg(true);
+
   }
 
   // チャットフォームの表示
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <h4>youtube Live URL</h4>
+      <form>
         <label>
           <textarea
-            rows='5'
+            rows='1'
             cols='50'
-            value={message}
+            value={youtubeUrl}
             onChange={handleMessageChange}
           />
         </label>
-        <div>
-          <button type="submit">質問する</button>
-        </div>
       </form>
-      {answer && (
+      <div>
+          <button onClick={reading_comment_start} disabled={readingCommentStartFlg}>コメント読み取り開始</button>
+        </div>
+      <div>
+          <button onClick={reading_comment_Stop} disabled={reading_comment_StopFlg}>コメント読み取り停止</button>
+        </div>
+      {youtubeCommentData && (
+        youtubeCommentData?.map((data) => (
+          <div key={data.id}>{data.snippet.textMessageDetails.messageText}</div>
+        ))
+      )}
+      {/* {answer && (
         <div>
           <h2>回答:</h2>
           <p>{answer}</p>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
